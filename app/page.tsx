3155dab34e61353689c1dcc91094c68e00b2912e
kjeom <jack.jin@kaia.io>
2025-02-11 17:54:07 +0900

    add feeDelegatedValueTransferWithMemo tx

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,10 @@ export default function Page() {
   const [provider, setProvider] = useState<any>(null);
   const [accounts, setAccounts] = useState<any>([]);
   const [fdvt, setFdvt] = useState<string>("");
+  const [fdvtm, setFdvtm] = useState<string>("");
   const [fdsc, setFdsc] = useState<string>("");
   const [vtTx, setVtTx] = useState({});
+  const [vtmTx, setVtmTx] = useState({});
   const [scTx, setScTx] = useState({});
 
   const connectWalletByDappPortalSDK = async () => {
@@ -77,6 +79,30 @@ export default function Page() {
     setFdsc(userSignedTx);
   };
 
+  const feeDelegatedValueTransferWithMemo = async () => {
+    if (!provider) {
+      alert("Please connect wallet first");
+      return;
+    }
+
+    const accountAddress = accounts[0];
+    const tx = {
+      typeInt: TxType.FeeDelegatedValueTransferMemo,
+      from: accountAddress,
+      to: accountAddress,
+      value: "0x10",
+      input: "0x1234567890abcdef",
+      feePayer: "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+    };
+    setVtmTx(tx);
+
+    const userSignedTx = await provider.request({
+      method: "kaia_signTransaction",
+      params: [tx],
+    });
+    setFdvtm(userSignedTx);
+  };
+
   return (
     <div className="flex flex-col gap-5 ml-10 mt-10">
       <div className="flex flex-row">
@@ -107,6 +133,28 @@ export default function Page() {
         <JSONPretty
           id="json-pretty"
           data={fdvt}
+          className="bg-slate-100 my-2 p-2"
+        ></JSONPretty>
+      </div>
+      <div className="flex flex-col">
+        <div>
+          <span>Fee Delegated Value Transfer with memo</span>
+          <button
+            onClick={feeDelegatedValueTransferWithMemo}
+            className="bg-slate-300  w-auto p-1 ml-5"
+          >
+            sign transaction
+          </button>
+        </div>
+        <JSONPretty
+          id="json-pretty"
+          data={vtmTx}
+          className="bg-slate-100 my-2 p-2"
+        ></JSONPretty>
+        <span>signed rlp encoded tx:</span>
+        <JSONPretty
+          id="json-pretty"
+          data={fdvtm}
           className="bg-slate-100 my-2 p-2"
         ></JSONPretty>
       </div>
